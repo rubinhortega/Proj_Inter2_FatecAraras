8ccefdacc3370d37dd2dc05f6f7b170dee797a6a
Marcel de Menezes Araujo <89671847+araujomarcel@users.noreply.github.com>
2022-05-26 22:05:34 -0300

    Add files via upload
    adição das páginas de comunidades

---
+++ site/src/ComunidadeAjuda.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="font-awesome.min.css" />
+    <link rel="stylesheet" href="jquery-ui.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+    <title>Projeto_09</title>
+</head>
+
+<body>
+    <section class="header-master">
+        <header>
+            <div class="container">
+                <div class="logo"></div>
+                <nav class="desktop">
+                    <ul>
+                        <li class="select"><span></span><a href="#zero">Home</a></li>
+                        <li><span></span><a href="#one">Informações</a></li>
+                        <li><span></span><a href="#two">Comunidades</a></li>
+                        <li><span></span><a href="#tree"></a>Orientações</li>
+                    </ul>
+                </nav>
+                <nav class="mobile">
+                    <i class="fa fa-bars"></i>
+                    <div class="clear"></div>
+                    <ul>
+                        <li><span></span><a href="#zero">Home</a></li>
+                        <li><span></span><a href="#one">Informações</a></li>
+                        <li><span></span><a href="#two">Comunidades</a></li>
+                        <li><span></span><a href="#tree">Duvidas</a></li>
+                        <li><span></span><a href="#four">Login</a></li>
+                    </ul>
+                </nav>
+                <div class="telefone-header">
+                    <li><span></span><a href="#one">Login</a></li>
+                </div>
+                <div class="clear"></div>
+                <!--Telefone-->
+            </div>
+            <!--container-->
+        </header>
+        <!--header-->
+        <div class="content-header">
+            <div class="container1">
+                <div class="one">
+                    <h2>
+                        Comunidades!
+                    </h2>
+                    <p>
+                    <li><a href="ComunidadeAjuda.php">Comunidade Ajuda</a></li><br>
+		         <li><a href="MaosAjudam.php">Comunidade Mãos Que Ajudam</a></li><br>
+		<li><a href=ComunidadeAnjos.php>Comunidade Anjos</a></li><br>
+		<li><a href="ComunidadeArcanjo.php">Comunidade Arcanjo</a></li><br>
+		<li><a href="ComunidadeFraternal.php">Comunidade Fraternal</a></li><br>
+                    </p>
+             
+                </div>
+                <!--one-->
+                <div class="two">
+                    <br><br><br>
+                <p style="text-align: justify;">Uma Organização sem fins lucrativos, de promoção ao desenvolvimento social que trabalha desde 2013, na assistência social e humanitária das pessoas em situação de necessidade.
+                O trabalho conta com o apoio financeiro de pessoas físicas que contribuem com um valor mensal denominadas associados e com comerciários que nos apoiam com doações. </p>
+                <img src="imagens/maos.png">
+                <img src="imagens/maos1.png">
+
+                    <div class="clear"></div>
+                </div>
+                <!--two-->
+                <div class="clear"></div>
+            </div>
+            <!--container-->
+        </div>
+        <!--content-header-->
+    </section>
+    <!--section-->
+
+    
+    
+    <section class="contato" id="tree">
+        <div class="container">
+            <h2>Entre em contato</h2>
+            <div class="box">
+                <p>Telefone: (xx) xxxx-xxxx</p>
+                <p>Email: contato@gamil.com</p>
+                <p>Endereço: Rua Jarbas Leme de Godoy
+                    <br>
+                 José Ometto- Araras/SP
+                </p>
+        </div>
+            <form>
+                <div class="form-group">
+                    <input type="text" name="nome" required placeholder="Digite seu nome">
+                </div>
+                <div
+                 class="form-group">
+                    <input type="text" name="email" required placeholder="Digite seu email">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="telefone" placeholder="Digite seu telefone">
+                </div>
+                <div class="form-group">
+                    <textarea name="mensagem" placeholder="Digite sua mensagem"></textarea>
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="enviar" value="Enviar">
+                </div>
+            </form>
+        </div>
+    </section>
+
+    <footer>
+        <img src="imagens/logo.png">
+    </footer>
+
+    <script src="https://kit.fontawesome.com/3c8e4257f7.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ site/src/php/informacoes.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="font-awesome.min.css" />
+    <link rel="stylesheet" href="jquery-ui.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+    <title>Projeto_09</title>
+</head>
+
+<body>
+    <section class="header-master">
+        <header>
+            <div class="container">
+                <div class="logo"></div>
+                <nav class="desktop">
+                    <ul>
+                        <li class="select"><span></span><a href="#zero">Home</a></li>
+                        <li><span></span><a href="#one">Informações</a></li>
+                        <li><span></span><a href="#two">Fazer Doações</a></li>
+                        <li><span></span><a href="#tree"></a>Duvidas</li>
+                    </ul>
+                </nav>
+                <nav class="mobile">
+                    <i class="fa fa-bars"></i>
+                    <div class="clear"></div>
+                    <ul>
+                        <li><span></span><a href="#zero">Home</a></li>
+                        <li><span></span><a href="#one">Informações</a></li>
+                        <li><span></span><a href="#two">Duvidas</a></li>
+                        <li><span></span><a href="#tree">Login</a></li>
+                    </ul>
+                </nav>
+                <div class="telefone-header">
+                    <span>Faça Seu Login</span>
+                </div>
+                <div class="clear"></div>
+                <!--Telefone-->
+            </div>
+            <!--container-->
+        </header>
+        <!--header-->
+        <div class="content-header">
+            <div class="container1">
+                <div class="one">
+                    <h2>
+                        Fique Dentro das Últimas Notícias
+                    </h2>
+                    <p>
+                        Aqui  além de você ter acesso as últimas noticías sobre os desastres que 
+                        ocorreram. Você também pode encontrar alguem desaperecido em um desastre!
+                        Ou procurar por algum desastre logo aqui abaixo:
+                    </p>
+                    <form>
+                        <input type="pesquisa" name="pesquisa" placeholder="Digite sua busca!" />
+                        <input type="submit" name="acao" value="Pesquisar">
+                    </form>
+                    <span>Ou ligue para a Defesa Civil da Região atingida</span>
+                </div>
+                <!--one-->
+                <div class="two">
+                    <img src="imagens/round_images/circle01.jpg">
+                    <img src="imagens/round_images/circle02.jpg">
+                    <img src="imagens/round_images/circle03.jpg">
+                    <img src="imagens/round_images/circle04.jpg">
+                    <div class="clear"></div>
+                </div>
+                <!--two-->
+                <div class="clear"></div>
+            </div>
+            <!--container-->
+        </div>
+        <!--content-header-->
+    </section>
+    <!--section-->
+
+    <section class="diferenciais" id="zero">
+        <div class="container">
+            <div class="diferencial-single">
+                <img src="imagens/icones/icone04.png">
+                <h2>Veja a Lista de <b>Desaparecido</b></h2>
+                <p>Desaparecido Nome e Sobrenome<br>
+                    Desaparecido Nome e Sobrenome<br>
+                    Desaparecido Nome e Sobrenome<br>
+                    Desaparecido Nome e Sobrenome<br>
+                    Desaparecido Nome e Sobrenome<br>
+                    Desaparecido Nome e Sobrenome<br>
+                </p>
+            </div>
+            <!--diferencial-single-->
+            <div class="diferencial-single">
+                <img src="imagens/icones/icone02.png">
+                <h2>Veja os Últimos <b>Desastres</b>
+                <p>Desastre de Petropolis<br>
+                    Desastre de Petropolis<br>
+                    Desastre de Petropolis<br>
+                    Desastre de Petropolis<br>
+                    Desastre de Petropolis<br>
+                    Desastre de Petropolis<br>
+                    Desastre de Petropolis<br>
+                </p>
+            </div>
+            <!--diferencial-single-->
+            <div class="diferencial-single">
+                <img src="imagens/icones/icone03.png">
+                <h2>Estas São as <b>Comunidades de Apoio</b> Cadastradas</h2>
+                <p>Comunidade Salva Vidas da Serra do Sol<br>
+                <p>Comunidade Salva Vidas da Serra do Sol<br>
+                <p>Comunidade Salva Vidas da Serra do Sol<br>
+                <p>Comunidade Salva Vidas da Serra do Sol<br>
+                <p>Comunidade Salva Vidas da Serra do Sol<br>
+                <p>Comunidade Salva Vidas da Serra do Sol<br>
+                </p>
+            </div>
+        </div>
+        <!--container-->
+    </section>
+    <!--section-->
+
+    <section class="sobre" id="one">
+        <div class="container3">
+            <div class="sobre-container">
+                <img src="imagens/rosto.jpg">
+                <div class="clear"></div>
+            </div>
+            <!--sobre-1-->
+            <div class="sobre-container text">
+                <h2>Desastres Naturais no Mundo!</h2>
+                <p><b>Sismo e Tsunami na Indonésia</b>: em 26 de dezembro de 2004,
+                    um terremoto de magnitude 9 devastou grande parte da costa oeste de Sumatra, na Indonésia. 
+                    O terceiro maior maremoto do mundo atingiu cerca de quinze países da região, resultando na morte de mais de 230 mil pessoas.</p><br>
+                    <p><b>Furacão Katrina</b>: em 29 de agosto de 2005, nos Estados Unidos, 
+                        surge um enorme furacão de categoria 5, responsável por destruir parte da região litorânea sul do país.
+                         A velocidade dos ventos ultrapassaram 280 quilômetros por hora e resultou na morte de duas mil pessoas</p><br>
+                         <p><b>Terremoto do Haiti</b> em 12 de janeiro de 2010, Porto Príncipe, 
+                             a capital do Haiti foi atingida por um terremoto de magnitude 7, 
+                             levando a morte de mais de 200 mil pessoas.</p><br>
+                <div class="clear"></div>
+            </div>
+            <!--sobre-2-->
+            <div class="clear"></div>
+        </div>
+        <!--container-->
+    </section>
+    <!--section-->
+
+    <section class="depoimento">
+        <div class="containe5">
+            <h2><i class="fas fa-quote-right a"></i>Não é o quanto fazemos, mas quanto amor colocamos naquilo que fazemos. Não é o quanto damos, mas quanto amor colocamos em dar.<i
+                    class="fas fa-quote-right"></i></h2>
+            <p>Madre Teresa de Calcutá</p>
+        </div>
+    </section>
+
+    <section class="etapas">
+        <div class="container5">
+            <h2>Como <b>sobreviver </b>a um desastre natural?</h2>
+            <div class="estapas__single um">
+                <div class="estapas__group">
+                    <img src="imagens/etapas/round_etapas_01.jpg">
+                    <p class="p1">Sobreviva a um desastre natural</p>
+                    <div class="clear"></div>
+                </div>
+                <!--estpas__group-->
+                <div class="estapas__group">
+                    <p class="p2">Sobreviva a um desastre natural</p>
+                    <img src="imagens/etapas/round_etapas_03.jpg">
+                    <div class="clear"></div>
+                </div>
+                <!--estpas__group-->
+                <div class="estapas__group">
+                    <img src="imagens/etapas/round_etapas_02.jpg">
+                    <p class="p3">Sobreviva a um desastre natural</p>
+                    <div class="clear"></div>
+                </div>
+                <!--estpas__group-->
+            </div>
+            <!--estpas__one-->
+            <div style="margin-top: 50px; padding: 0 30px;" class="estapas__single tree">
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <div class="line"></div>
+                        <span>1</span>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Matenha-se Acordado</h3>
+                        <p>Muitos desastres acontecem enquanto as pessoas estão dormindo. Chuvas que duram a noite toda e outros indícios são sinais  graves de que ele pode acontecer. 
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <span>2</span>
+                        <div class="line"></div>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Aumente a Atenção</h3>
+                        <p>Fique atento aos sons de árvores quebrando, ventos fortes ou pedras rolando terreno abaixo. Isso acontece quando a terra vira lama e arrasta tudo que está pela frente. 
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <div class="line"></div>
+                        <span>3</span>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Vire uma bolinha </h3>
+                        <p>Se não há mais o que fazer, e você está no meio de uma situação de perigo, coloque sua cabeça no chão e a proteja com suas mãos. A posição acima resguarda suas partes mais frágeis. 
+                    </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <span>4</span>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Disque 193</h3>
+                        <p>Se não estiver preso, pegue o celular e ligue para o bombeiro. Fale pouco para não acabar a bateria. Explique onde estava quando o desastre aconteceu e espere pelo resgate. 
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+            </div>
+            <!--estpas__two-->
+            <div class="clear"></div>
+        </div>
+        <!--etapas-->
+    </section>
+    <!--section-->
+
+    
+    <section class="contato" id="tree">
+        <div class="container">
+            <h2>Entre em contato</h2>
+            <div class="box">
+                <p>Telefone: (xx) xxxx-xxxx</p>
+                <p>Email: contato@gamil.com</p>
+                <p>Endereço: Rua Jarbas Leme de Godoy
+                    <br>
+                 José Ometto- Araras/SP
+                </p>
+        </div>
+            <form>
+                <div class="form-group">
+                    <input type="text" name="nome" required placeholder="Digite seu nome">
+                </div>
+                <div
+                 class="form-group">
+                    <input type="text" name="email" required placeholder="Digite seu email">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="telefone" placeholder="Digite seu telefone">
+                </div>
+                <div class="form-group">
+                    <textarea name="mensagem" placeholder="Digite sua mensagem"></textarea>
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="enviar" value="Enviar">
+                </div>
+            </form>
+        </div>
+    </section>
+
+    <footer>
+        <img src="imagens/logo.png">
+    </footer>
+
+    <script src="https://kit.fontawesome.com/3c8e4257f7.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
