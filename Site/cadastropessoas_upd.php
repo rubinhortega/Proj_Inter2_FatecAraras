4fe6b585228d3d788eec2c0e6f6a147e34469b24
Danielle Ferreira <103910912+DannielleFerreira@users.noreply.github.com>
2022-06-08 04:57:20 -0300

    Update cadastropessoas_upd.php

--- Site/cadastropessoas_upd.php
+++ Site/cadastropessoas_upd.php
@@ -106,7 +106,7 @@
 <body>
     <div class="container">
         <div class="form-image">
-            <img src="img/Global warming-rafiki.svg" alt="">
+            <img src="imagens/Global warming-rafiki.svg" alt="">
         </div>
         <div class="form">
             <form action="cadastropessoas_upd.php" method="POST">
