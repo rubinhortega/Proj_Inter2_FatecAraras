7f591458af3f67c2b5595b7f61e8e586c2bc8a1d
Danielle Ferreira <103910912+DannielleFerreira@users.noreply.github.com>
2022-06-08 04:59:01 -0300

    Update cadastrovitimas_add.php

--- Site/cadastrovitimas_add.php
+++ Site/cadastrovitimas_add.php
@@ -42,7 +42,7 @@
 <body>
     <div class="container">
         <div class="form-image">
-            <img src="img/Hospital bed-amico.svg" alt="">
+            <img src="imagens/Hospital bed-amico.svg" alt="">
         </div>
         <div class="form">
             <form action="cadastrovitimas_add.php" method="POST">
