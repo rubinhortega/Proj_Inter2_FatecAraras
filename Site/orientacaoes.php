2cb0fb45b3b478698994ef2d23b6dad89213ed1c
Marcel de Menezes Araujo <89671847+araujomarcel@users.noreply.github.com>
2022-06-07 21:40:41 -0300

    add site
    Páginas de index;comunidades;orientações adicionadas!

---
+++ Site/orientacaoes.php
@@ -0,0 +1,465 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="font-awesome.min.css" />
+    <link rel="stylesheet" href="jquery-ui.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+    <title>SADN</title>
+</head>
+
+<body>
+
+    <section class="header-master">
+        <header>
+        <div class="container">
+                <div class="logo">
+                <img src="imagens/logo.png">
+                </div>
+                <nav class="desktop">
+                    <ul> 
+                    
+                </div>
+                <nav class="desktop">
+                    <ul>
+                        <li class="select"><span></span><a href="index.html">Home</a></li>
+                        <li><span></span><a href="informacoes.php">Informações</a></li>
+                        <li><span></span><a href="comunidades.php">Comunidades</a></li>
+                        <li><span></span><a href="orientacaoes.php">Orietações</a></li>
+                        <li><span></span><a href="login.php">Login</a></li>
+                    </ul>
+                </nav>
+                <nav class="mobile">
+                    <i class="fa fa-bars"></i>
+                    <div class="clear"></div>
+                    <ul>
+                        <li><span></span><a href="index.html">Home</a></li>
+                        <li><span></span><a href="informacoes.php">Informações</a></li>
+                        <li><span></span><a href="comunidades.php">Comunidades</a></li>
+                        <li><span></span><a href="orientacaoes.php">Orientações</a></li>
+                        
+                    </ul>
+                </nav>
+                
+                <div class="clear"></div>
+                <!--Telefone-->
+            </div>
+            <!--container-->
+        </header>
+        <!--header-->
+        <div class="content-header">
+        <div class="content-header">
+            <div class="container1">
+            <section class="etapas">
+        <div class="container5">
+            <h2><b>Orientações de </b>Sobrevivência em Tornados</h2>
+            <div class="estapas__single um">    
+                <div class="estapas__group">
+                    <img src="imagens/etapas/tornado1.jpg">
+                    <p class="p1"></p>
+                    <div class="clear"></div>
+                </div>
+                <!--estpas__group-->
+                <div class="estapas__group">
+                    <p class="p2">Como Sobreviver em um </br>Desastre Natural</p>
+                    <img src="imagens/etapas/tornado2.jpg">
+                    <div class="clear"></div>
+                </div>
+                <!--estpas__group-->
+                <div class="estapas__group">
+                    <img src="imagens/etapas/tornado3.jpg">
+                    <p class="p3"></p>
+                    <div class="clear"></div>
+                </div>
+                <!--estpas__group-->
+            </div>
+            <!--estpas__one-->
+            <div style="margin-top: 50px; padding: 0 30px;" class="estapas__single tree">
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <div class="line"></div>
+                        <span>1</span>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Esteja Atento</h3>
+                        <p>Esteja atento aos boletins meteorológicos e às notícias locais transmitidas
+                        nas emissoras de rádio e televisão 
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <span>2</span>
+                        <div class="line"></div>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Prepare-se Com Um Kit</h3>
+                        <p>Tenha um kit em casa com lanterna, pilhas, roupas, medicamentos, comidas
+                        não perecíveis e água potável
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <div class="line"></div>
+                        <span>3</span>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Abrigar-se </h3>
+                        <p>O abrigo subterrâneo (porão) é a melhor proteção individual </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <span>4</span>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Caso não tenha um porão, onde posso ficar?</h3>
+                        <p>Na ausência de um porão, procure ficar longe das janelas e portas, de
+                        preferência no corredor, banheiro ou armário, sempre no piso mais baixo da
+                        casa
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+            </div>
+            <!--estpas__two-->
+            <div class="clear"></div>
+        </div>
+        <!--etapas-->
+    </section>
+    <!--section-->
+
+                <div class="clear"></div>
+            
+            <!--container-->
+        </div>
+        <!--content-header-->
+    </section>
+    <!--section-->
+
+    <section class="diferenciais" id="zero">
+        <div class="container">
+            <div class="diferencial-single">
+            </div>
+            <!--diferencial-single-->
+            <div class="diferencial-single">
+              
+                
+            </div>
+            <!--diferencial-single-->
+            <div class="diferencial-single">
+              
+            </div>
+        </div>
+        <!--container-->
+    </section>
+    <!--section-->
+
+    <section class="sobre" id="one">
+        <div class="container3">
+            <div class="sobre-container">
+                <div class="clear"></div>
+            </div>
+            <!--sobre-1-->
+            <div class="sobre-container text">
+                <div class="clear"></div>
+            </div>
+            <!--sobre-2-->
+            <div class="clear"></div>
+        </div>
+        <!--container-->
+        <div class="content-header">
+            <div class="container1">
+            <section class="etapas">
+        <div class="container5">
+            <h2><b>Orientações de </b>Sobrevivência em Fortes Chuvas</h2>
+            <div class="estapas__single um">    
+                <div class="estapas__group">
+                    <img src="imagens/etapas/chuva1.jpg">
+                    <p class="p1"></p>
+                    <div class="clear"></div>
+                </div>
+                <!--estpas__group-->
+                <div class="estapas__group">
+                    <p class="p2">Como Sobreviver em um </br>Desastre Natural</p>
+                    <img src="imagens/etapas/chuva2.jpg">
+                    <div class="clear"></div>
+                </div>
+                <!--estpas__group-->
+                <div class="estapas__group">
+                    <img src="imagens/etapas/chuva3.jpg">
+                    <p class="p3"></p>
+                    <div class="clear"></div>
+                </div>
+                <!--estpas__group-->
+            </div>
+            <!--estpas__one-->
+            <div style="margin-top: 50px; padding: 0 30px;" class="estapas__single tree">
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <div class="line"></div>
+                        <span>1</span>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Matenha a Calma</h3>
+                        <p>Antes de tudo, proteja a sua vida, a de seus familiares e amigos,
+                        permanecendo calmo, e procure não se deslocar. Fique atento às informações
+                        fornecidas pelos órgãos competentes através da mídia. 
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <span>2</span>
+                        <div class="line"></div>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Peça Ajuda</h3>
+                        <p>Em casos mais sérios, peça ajuda a DEFESA CIVIL (telefone 199) e ao
+                        CORPO DE BOMBEIROS (telefone 193) 
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <div class="line"></div>
+                        <span>3</span>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Cuidado com Alimentação </h3>
+                        <p>Não coma alimentos que tiveram contato com as águas e não beba água da
+                        inundação (em hipótese alguma) 
+                    </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <span>4</span>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Quando Posso Voltar Para Casa?</h3>
+                        <p>Caso tenha sido retirado de sua residência (evacuação ou salvamento) não
+                        volte para casa até as águas baixarem e o caminho estar seguro
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+            </div>
+            <!--estpas__two-->
+            <div class="clear"></div>
+        </div>
+        <!--etapas-->
+    </section>
+    <!--section-->
+    </section>
+    <!--section-->
+
+    <section class="depoimento">
+        <div class="containe5">
+            <h2><i class=""></i><i
+                    class=""></i></h2>
+            <p></p>
+        </div>
+        <section class="etapas">
+        <div class="container5">
+            <h2><b>Orientações de </b>Sobrevivência em  Delizamentos</h2>
+            <div class="estapas__single um">    
+                <div class="estapas__group">
+                    <img src="imagens/etapas/deslizamento2.jpg">
+                    <p class="p1"></p>
+                    <div class="clear"></div>
+                </div>
+                <!--estpas__group-->
+                <div class="estapas__group">
+                    <p class="p2">Como Sobreviver em um </br>Desastre Natural</p>
+                    <img src="imagens/etapas/deslizamento1.jpg">
+                    <div class="clear"></div>
+                </div>
+                <!--estpas__group-->
+                <div class="estapas__group">
+                    <img src="imagens/etapas/deslizamento3.jpg">
+                    <p class="p3"></p>
+                    <div class="clear"></div>
+                </div>
+                <!--estpas__group-->
+            </div>
+            <!--estpas__one-->
+            <div style="margin-top: 50px; padding: 0 30px;" class="estapas__single tree">
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <div class="line"></div>
+                        <span>1</span>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Construções de Casas</h3>
+                        <p>Evite construir em encostas muito íngremes e próximo a cursos d`água em
+                        vales muito profundos
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <span>2</span>
+                        <div class="line"></div>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Esteja Atento aos Noticiários</h3>
+                        <p>Esteja atento aos boletins meteorológicos e as notícias de rádio e TV de sua
+                        região. Lembre-se: as chuvas intensas podem facilmente desencadear os
+                        escorregamentos
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <div class="line"></div>
+                        <span>3</span>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Atençãos aos Barulhos</h3>
+                        <p>Se você ouvir barulhos estranhos como árvores caindo e paredes rachando,
+                        saia de casa rapidamente, avise seus vizinhos e contate os órgãos
+                        responsáveis
+                    </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+                <div class="etapas__group2">
+                    <div class="circle">
+                        <span>4</span>
+                        <div class="clear"></div>
+                    </div>
+                    <!--cricle-->
+                    <div class="etapas__descricao">
+                        <h3>Caso seja Atingido</h3>
+                        <p>Caso atingido pelo fluxo de lama, tente colocar o queixo entre os joelhos e
+                        proteja sua cabeça com as duas mãos. A forma de bola (esfera) lhe ajudará a
+                        proteger melhor o seu corpo
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                    <!--estpas__descricao-->
+                </div>
+                <!--estpas__group2-->
+            </div>
+            <!--estpas__two-->
+            <div class="clear"></div>
+        </div>
+        <!--etapas-->
+    </section>
+    <!--section-->
+                    <div class="clear"></div>
+                </div>
+                <!--two-->
+                <div class="clear"></div>
+            </div>
+            <!--container-->
+        </div>
+        <!--content-header-->
+    </section>
+    <!--section-->
+    </section>
+
+ 
+    <!--section-->
+    <section class="contato" id="tree">
+        <div class="container">
+            <h2>Entre em contato</h2>
+            <div class="box">
+                <p>Telefone: (xx) xxxx-xxxx</p>
+                <p>Email: contato@gamil.com</p>
+                <p>Endereço: Rua Jarbas Leme de Godoy
+                    <br>
+                 José Ometto- Araras/SP
+                </p>
+        </div>
+            <form>
+                <div class="form-group">
+                    <input type="text" name="nome" required placeholder="Digite seu nome">
+                </div>
+                <div
+                 class="form-group">
+                    <input type="text" name="email" required placeholder="Digite seu email">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="telefone" placeholder="Digite seu telefone">
+                </div>
+                <div class="form-group">
+                    <textarea name="mensagem" placeholder="Digite sua mensagem"></textarea>
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="enviar" value="Enviar">
+                </div>
+            </form>
+        </div>
+    </section>
+
+    <footer>
+        <img src="imagens/logo.png">
+    </footer>
+
+    <script src="https://kit.fontawesome.com/3c8e4257f7.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
