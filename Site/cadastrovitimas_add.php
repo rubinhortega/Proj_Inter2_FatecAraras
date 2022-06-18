56a3ba623dec091d9f0e7cc244402136edc38bde
Danielle Ferreira <103910912+DannielleFerreira@users.noreply.github.com>
2022-06-18 16:33:41 -0300

    Update cadastrovitimas_add.php

--- Site/cadastrovitimas_add.php
+++ Site/cadastrovitimas_add.php
@@ -3,20 +3,22 @@
 
     if (isset($_POST['gravar']))
     {   
-        $INSERIDOPOR = $_POST['nomedes'];
+        $INSERIDOPOR = $_POST['nomein'];
         $NOME = $_POST['nomevit'];
         $CPF  = $_POST['cpf'];
         $IDADE = $_POST['idade'];
-        $OBTO = '';
-        $ENCERRADO = '';
+        $OBTO = $_POST['obto'];
+        if (empty($OBTO)) {
+            $OBTO = 'N';
+        }
+        $ENCERRADO = 'N';
         $DATA_HORA = $_POST['datevit'];
         $CARACTERISTICAS = $_POST['caract'];
      
 
         $V = new Vitima;
         
-        if ($V->VitimaADD($ID_DESASTRE,
-                          $ID_UNIDATENDTO,$INSERIDOPOR, $NOME, $CPF, $IDADE, $OBTO, $ENCERRADO, $DATA_HORA, $CARACTERISTICAS, $erro) > 0)
+        if ($V->VitimaADD($ID_DESASTRE, $ID_UNIDATENDTO, $INSERIDOPOR, $NOME, $CPF, $IDADE, $OBTO, $ENCERRADO, $DATA_HORA, $CARACTERISTICAS, $erro) > 0)
         {
             //echo 'Inserido com sucesso !';
             header("location: index.php");
@@ -41,6 +43,10 @@
 </head>
 <body>
     <div class="container">
+        <div class="logo">
+           <h1><img src="imagens/logo.png" alt="">SADN</h1>
+           <p>Sistema de Apoio a Desastre Natural</p>
+        </div>
         <div class="form-image">
             <img src="imagens/Hospital bed-amico.svg" alt="">
         </div>
@@ -53,8 +59,8 @@
                 </div>
                 <div class="input-group">
                     <div class="input-box">
-                        <label for="nomedes">Nome do desastre</label>
-                        <input id="nome" type="text" nome="nomedes" placeholder="Digite o nome do desastre">
+                        <label for="nomein">Nome do Inseridor</label>
+                        <input id="nomein" type="text" nome="nomein" placeholder="Digite o nome do desastre" required>
                     </div>
                     <div class="input-box">
                         <label for="name">Nome da vitima</label>
@@ -62,17 +68,15 @@
                     </div>
                     <div class="input-box">
                         <label for="cpf">CPF</label>
-                        <input id="cpf" type="text" name="cpf" placeholder="xxx.xxx.xxx-xx">
+                        <input id="cpf" type="text" name="cpf" placeholder="xxx.xxx.xxx-xx" required>
                     </div>
                     <div class="input-box">
                         <label for="idadevit">Idade</label>
-                        <input id="idadevit" type="text" name="idadevit" placeholder="Digite a idade da vitima"
-                            required>
+                        <input id="idadevit" type="text" name="idadevit" placeholder="Digite a idade da vitima" required>
                     </div>
                     <div class="input-box">
                         <label for="datevit">Data e hora</label>
-                        <input id="datevit" type="datetime
-                        " name="datevit" required>
+                        <input id="datevit" type="date" name="datevit" required>
                     </div>
                     <div class="input-box">
                         <label for="cidade">Cidade</label>
@@ -83,27 +87,26 @@
                         <input id="uf" type="text" name="uf" placeholder="Digite seu estado" required>
                     </div>
                     <div class="input-box">
-                        <input type="checkbox" id="obito
-                        " name="obito" checked>
                         <label for="obito" style="color: #fff;">Óbito?</label>
-                    </div>
-                    <div class="input-box">
-                        <label for="caract">Caracteristicas</label>
-                        <textarea id="caract" type="text" name="caract"> Digite as Caracteristicas da vitima</textarea>
-                    </div>
-                    <div class="continue-button ">
-                        <button><a href="gravar" id="gravar" name="gravar">Cadastrar</a></button>
+                        <input type="checkbox" id="obito" name="obito">
                     </div>
                     <div class="input-box">
                         <label for="unidatend">Unidade de Atendimento</label>
-                        <select name="unidatend" id="unidatend" aria-placeholder="Unidade de Atendimento">
-                            <option value="">Unidade de Atendimento</option>
+                        <select name="unidatend" id="unidatend" placeholder="Unidade de Atendimento">
                             <option value="">Santa Casa</option>
                             <option value="">Unimed</option>
                             <option value="">Amor saúde</option>
                             <option value="">Pró-saúde</option>
                             <option value="">UPA</option>
-                        </selec>
+                        </select>
+                    </div>
+                    <div class="input-box">
+                        <label for="caract">Caracteristicas</label>
+                        <textarea id="caract" type="text" name="caract" placeholder="Digite as Caracteristicas da vitima"></textarea>
+                    </div>
+                    <div class="continue-button ">
+                       <button type="submit" name="gravar" id="gravar">Cadastrar</button>
+                       <button type="submit"><a href="index.php">Voltar</a></button>
                     </div>
                 </div>
             </form>
