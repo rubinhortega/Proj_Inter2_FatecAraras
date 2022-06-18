50317e072e6b2fb8da725f351fe1da86779db96e
Danielle Ferreira <103910912+DannielleFerreira@users.noreply.github.com>
2022-06-18 16:54:31 -0300

    Update cadunidadeatend_upd.php

--- Site/cadunidadeatend_upd.php
+++ Site/cadunidadeatend_upd.php
@@ -1,9 +1,9 @@
 <?php
     include_once 'classes\inidatendto.php';
     
-    if (!empty($_GET['id']))
+    if (!empty($_GET['ID_UNIDATENDTO']))
     {
-        $id = $_GET['id'];
+        $id = $_GET['ID_UNIDATENDTO'];
 
         $U = new UnidAtendto;
         $linhas = $U->UnidAtendtoExiste($ID_UNIDATENDTO, $erro);
@@ -20,8 +20,8 @@
             foreach ($linhas as $lin)
             {
                 $ID_UNIDATENDTO = $lin['ID_UNIDATENDTO'];
-                $NOME = $lin['NOME'];
                 $ATIVO = $lin['ATIVO'];
+                $NOME = $lin['NOME'];
                 $FONES = $lin['FONES'];
                 $EMAIL = $lin['EMAIL'];
                 $SITE  = $lin['SITE'];
@@ -41,8 +41,8 @@
     if (isset($_POST['gravar']))
     {
         $ID = $_POST['ID_UNIDATENDTO'];
-        $NOME   = $_POST['nome'];
         $ATIVO  = 'S';
+        $NOME   = $_POST['nome'];
         $FONES  = $_POST['fone'];
         $EMAIL  = $_POST['email'];
         $SITE   = $_POST['site'];
@@ -97,73 +97,75 @@
 
 <body>
     <div class="container">
+        <div class="logo">
+           <h1><img src="imagens/logo.png" alt="">SADN
+           </h1>
+           <p>Sistema de Apoio a Desastre Natural</p>
+        </div>
         <div class="form-image">
             <img src="imagens/Hospital building-rafiki.svg" alt="">
         </div>
         <div class="form">
             <form action="cadunidadeatend_upd.php" method="POST">
-                <FIEldset>
-                    <div class="form-header">
-                        <div class="title">
-                            <h1>Atualizar unidade de atendimento</h1>
-                        </div>
+                <div class="form-header">
+                    <div class="title">
+                        <h1>Atualizar unidade de atendimento</h1>
                     </div>
-                    <div class="input-group">
-                        <div class="input-box">
-                            <label for="nome">Nome</label>
-                            <input id="nome" type="text" name="nome" placeholder="Digite o nome da uniade de atendimento" value="<?php echo $NOME?>"required>
-                        </div>
-                        <div class="input-box">
-                            <label for="nomeresp">Nome do responsável</label>
-                            <input id="nomeresp" type="text" name="nomeresp" placeholder="Nome do responsável" value="<?php echo $NOME_RESP ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="emailcua">E-mail</label>
-                            <input id="emailcua" type="email" name="emailcua" placeholder="Digite o e-mail" value="<?php echo $EMAIL ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="log">Logradouro</label>
-                            <input id="log" type="text" name="log" placeholder="Digite o logradouro" value="<?php echo $END_LOG ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="n">Nº</label>
-                            <input id="n" type="text" name="n" placeholder="Digite o número do local de atendimento" value="<?php echo $END_NUM ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="bairro">Bairro</label>
-                            <input id="bairro" type="text" name="bairro" placeholder="Digite o bairro" value="<?php echo $END_BAI ?>"required>
-                        </div>
-                        <div class="input-box">
-                            <label for="cidade">Cidade</label>
-                            <input id="cidade" type="text" name="cidade" placeholder="Digite a cidade" value="<?php echo $END_CID ?>"required>
-                        </div>
-                        <div class="input-box">
-                            <label for="uf">UF</label>
-                            <input id="uf" type="text" name="uf" placeholder="Digite o estado" value="<?php echo $END_UF ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="cep">CEP</label>
-                            <input id="cep" type="text" name="cep" placeholder="xx.xxx-xxx" value="<?php echo $END_UF ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="com">Complemento</label>
-                            <input id="com" type="text" name="com" value="<?php echo $END_COM ?>" placeholder="Digite informções extras">
-                        </div>
-                        <div class="input-box">
-                            <label for="fone">Contatos</label>
-                            <textarea id="fone" type="text" name="fone" value="<?php echo $FONES ?>"> Contatos da unidade de atendimento</textarea><br><br>
-                        </div>
-                        <div class="input-box">
-                            <label for="Obs">Observações</label>
-                            <textarea id="Obs" type="text" name="Obs" value="<?php echo $OBS ?>"> Observações</textarea><br><br>
-                        </div>
-                        <div class="continue-button ">
-                            <input type="hidden" name="id" id="id" value="<?php echo $id ?>">
-                            <input type="submit" name="gravar" id="gravar" class="btn btnok" value="Gravar">
-                            <input type="button" name="cancel" id="cancel" class="btn btnvoltar" value="Voltar" onclick="history.go(-1);">
-                        </div>
+                </div>
+                <div class="input-group">
+                    <div class="input-box">
+                        <label for="nome">Nome</label>
+                        <input id="nome" type="text" name="nome" placeholder="Digite o nome da uniade de atendimento" value="<?php echo $NOME?>"required>
                     </div>
-                </FIEldset>
+                    <div class="input-box">
+                        <label for="nomeresp">Nome do responsável</label>
+                        <input id="nomeresp" type="text" name="nomeresp" placeholder="Nome do responsável" value="<?php echo $NOME_RESP ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="emailcua">E-mail</label>
+                        <input id="emailcua" type="email" name="emailcua" placeholder="Digite o e-mail" value="<?php echo $EMAIL ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="log">Logradouro</label>
+                        <input id="log" type="text" name="log" placeholder="Digite o logradouro" value="<?php echo $END_LOG ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="n">Nº</label>
+                        <input id="n" type="text" name="n" placeholder="Digite o número do local de atendimento" value="<?php echo $END_NUM ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="bairro">Bairro</label>
+                        <input id="bairro" type="text" name="bairro" placeholder="Digite o bairro" value="<?php echo $END_BAI ?>"required>
+                    </div>
+                    <div class="input-box">
+                        <label for="cidade">Cidade</label>
+                        <input id="cidade" type="text" name="cidade" placeholder="Digite a cidade" value="<?php echo $END_CID ?>"required>
+                    </div>
+                    <div class="input-box">
+                        <label for="uf">UF</label>
+                        <input id="uf" type="text" name="uf" placeholder="Digite o estado" value="<?php echo $END_UF ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="cep">CEP</label>
+                        <input id="cep" type="text" name="cep" placeholder="xx.xxx-xxx" value="<?php echo $END_UF ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="com">Complemento</label>
+                        <input id="com" type="text" name="com" value="<?php echo $END_COM ?>" placeholder="Digite informções extras">
+                    </div>
+                    <div class="input-box">
+                        <label for="fone">Contatos</label>
+                        <textarea id="fone" type="text" name="fone" value="<?php echo $FONES ?>" placeholder="Contatos da unidade de atendimento"></textarea>
+                    </div>
+                    <div class="input-box">
+                        <label for="Obs">Observações</label>
+                        <textarea id="Obs" type="text" name="Obs" value="<?php echo $OBS ?>" placeholder="Observações"></textarea>
+                    </div>
+                    <div class="continue-button">
+                       <button type="submit" name="gravar" id="gravar">Cadastrar</button>
+                       <button type="submit"><a href="index.php">Voltar</a></button>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
