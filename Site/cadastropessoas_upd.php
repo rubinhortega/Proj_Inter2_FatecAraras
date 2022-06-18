e5480ab59775eb7ddd1a09e4101e171d097f5ce5
Danielle Ferreira <103910912+DannielleFerreira@users.noreply.github.com>
2022-06-18 16:30:32 -0300

    Update cadastropessoas_upd.php

--- Site/cadastropessoas_upd.php
+++ Site/cadastropessoas_upd.php
@@ -1,9 +1,9 @@
 <?php
     include_once 'classes\pessoa.php';
     
-    if (!empty($_GET['id']))
+    if (!empty($_GET['ID_PESSOA']))
     {
-        $id = $_GET['id'];
+        $id = $_GET['ID_PESSOA'];
 
         $P = new Pessoa;
         $linhas = $P->PessoaExiste($ID_PESSOA, $erro);
@@ -47,11 +47,17 @@
         $CPF     = $_POST['cpf'];
         $NOME    = $_POST['nome'];
         $FONE    = $_POST['fone'];
-        $EHWHATS = 'S';
+        $EHWHATS = $_POST['what'];
+        if (empty($EHWHATS)) {
+            $EHWHATS = 'N';
+        }
         $SENHA   =$_POST['password'];
         $EMAIL   =$_POST['email'];
         $ATIVO   = 'S';
-        $EHOFICIAL = 'N';
+        $EHOFICIAL = $_POST['oficialck'];
+        if (empty($EHOFICIAL)) {
+            $EHOFICIAL = 'N';
+        }
         $LIB_CADDESASTRE =$_POST['fone'];
         $REGISTRO_OFICIAL =$_POST['oficial'];
         $CONTATOS =$_POST['contatos'];
@@ -105,87 +111,91 @@
 </head>
 <body>
     <div class="container">
+        <div class="logo">
+           <h1><img src="imagens/logo.png" alt="">SADN
+           </h1>
+           <p>Sistema de Apoio a Desastre Natural</p>
+        </div>
         <div class="form-image">
             <img src="imagens/Global warming-rafiki.svg" alt="">
         </div>
         <div class="form">
-            <form action="cadastropessoas_upd.php" method="POST">
-                <fieldset>
-                    <div class="form-header">
+            <form action="cadastropessoas_add.php" method="POST">
+                <div class="form-header">
                     <div class="title">
-                            <h1>Atualizar Cadastro</h1>
+                        <h1>Cadastra-se</h1>
                     </div>
+                </div>
+                <div class="input-group">
+                    <div class="input-box" >
+                        <label for="nome">Nome Completo</label>
+                        <input id="nome" type="text" name="nome"  placeholder="Digite seu nome Completo" value="<?php echo $NOME ?>" required>
                     </div>
-                    <div class="input-group">
-                        <div class="input-box" >
-                            <label for="nome">Nome Completo</label>
-                            <input id="nome" type="text" name="nome" placeholder="Digite seu nome Completo"  value="<?php echo $NOME ?>" required>
-                        </div>
-                            <div class="input-box">
-                            <label for="cpf">CPF</label>
-                            <input id="cpf" type="hidden" name="cpf" value="<?php echo $CPF ?>"placeholder="xxx.xxx.xxx-xx">
-                        </div>
-                        <div class="input-box">
-                            <input type="checkbox" name="what[]" value=on>Tem WhatsApp?<br>
-                        </div>
-                        <div class="input-box">
-                            <label for="logradouro">Logradouro</label>
-                            <input id="logradouro" type="text" name="logradouro" placeholder="Digite o seu logradouro" value="<?php echo $END_LOG ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="n">Nº</label>
-                            <input id="n" type="text" name="n" placeholder="Digite o número da sua recidência" value="<?php echo $END_NUM ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="bairro">Bairro</label>
-                            <input id="bairro" type="text" name="bairro" placeholder="Digite seu bairro" value="<?php echo $END_BAI ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="cidade">Cidade</label>
-                            <input id="cidade" type="text" name="cidade" placeholder="Digite sua cidade" value="<?php echo $END_CID ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="uf">UF</label>
-                            <input id="uf" type="text" name="uf" placeholder="Digite seu estado" value="<?php echo $END_UF ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="cep">CEP</label>
-                            <input id="cep" type="text" name="cep" placeholder="xx.xxx-xxx" value="<?php echo $CPF ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="complemento">Complemento</label>
-                            <input id="complemento" type="text" name="complemento" value="<?php echo $END_COM ?>">
-                        </div>
-                        <div class="input-box">
-                            <label for="email">E-mail</label>
-                            <input id="email" type="email" name="email" placeholder="Digite o e-mail" value="<?php echo $EMAIL ?>" required>
-                        </div>
-                        <div class="input-box">
-                            <label for="password">Senha</label>
-                            <input id="password" type="password" name="password" placeholder="Digite a senha" value="<?php echo $SENHA ?>"required>
-                        </div>
-                        <div class="input-box">
-                            <label for="confirmpassword">Confirme a senha</label>
-                            <input id="confirmpassword" type="password" name="comfirmpassword" placeholder="Digite a senha" value="<?php echo $SENHA ?>"required>
-                        </div>
-                        <div class="input-box">
-                            <label for="oficial">Número de registro</label>
-                            <input id="oficial" type="text" name="oficial" placeholder="Digite o número do seu registro oficial">
-                            <input type="checkbox" id="oficial" name="N" value="oficial">
-                            <label for="oficial">Você é um oficial.</label>
-                        </div>
-                        <div class="input-box">
-                            <label for="contatos">Contatos</label>
-                            <textarea id="contatos" type="text" name="contatos" value="<?php echo $CONTATOS ?>"> Contatos de familiares </textarea>
-                        </div>
-                        <br><br>
-                        <div class="continue-button ">
-                            <input type="hidden" name="id" id="id" value="<?php echo $id ?>">
-                            <input type="submit" name="gravar" id="gravar" class="btn btnok" value="Gravar">
-                            <input type="button" name="cancel" id="cancel" class="btn btnvoltar" value="Voltar" onclick="history.go(-1);">
-                        </div>
+                    <div class="input-box">
+                        <label for="cpf">CPF</label>
+                        <input id="cpf" type="text" name="cpf"  placeholder="xxx.xxx.xxx-xx" value="<?php echo $CPF ?>" required>
                     </div>
-                </fieldset>
+                    <div class="input-box">
+                        <label for="fone">Celular</label>
+                        <input id="fone" type="tel" name="fone" placeholder="(xx) xxxxx-xxxx" value="<?php echo $FONE ?>" required>
+                        <label for="what">Tem WhatsApp.</label>
+                        <input type="checkbox" id="what" name="what" value="S" checked>
+                    </div>
+                    <div class="input-box">
+                        <label for="logradouro">Logradouro</label>
+                        <input id="logradouro" type="text" name="logradouro"   placeholder="Digite o seu logradouro" value="<?php echo $END_LOG ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="n">Nº</label>
+                        <input id="n" type="text" name="n"  placeholder="Digite o número da sua recidência" value="<?php echo $END_NUM ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="bairro">Bairro</label>
+                        <input id="bairro" type="text" name="bairro"  placeholder="Digite seu bairro" value="<?php echo $END_BAI ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="cidade">Cidade</label>
+                        <input id="cidade" type="text" name="cidade"  placeholder="Digite sua cidade" value="<?php echo $END_CID ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="uf">UF</label>
+                        <input id="uf" type="text" name="uf"  placeholder="Digite seu estado" value="<?php echo $END_CID ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="cep">CEP</label>
+                        <input id="cep" type="text" name="cep"  placeholder="xx.xxx-xxx" value="<?php echo $END_CEP ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="complemento">Complemento</label>
+                        <input id="complemento" type="text" name="complemento" value="<?php echo $END_COM ?>">
+                    </div>
+                    <div class="input-box">
+                        <label for="email">E-mail</label>
+                        <input id="email" type="email" name="email"  placeholder="Digite o e-mail" value="<?php echo $EMAIL ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="password">Senha</label>
+                        <input id="password" type="password" name="password"  placeholder="Digite a senha" value="<?php echo $SENHA ?>" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="confirmpassword">Confirme a senha</label>
+                        <input id="confirmpassword" type="password" name="comfirmpassword" placeholder="Digite a senha" >
+                    </div>
+                    <div class="input-box">
+                        <label for="oficial">Número de registro</label>
+                        <input id="oficial" type="text" name="oficial"  placeholder="Digite o número do seu registro oficial" value="<?php echo $REGISTRO_OFICIAL ?>">
+                        <label for="oficial">Eu sou oficial.</label>
+                        <input type="checkbox" id="oficialck" name="oficialck" value="S" checked>
+                    </div>
+                    <div class="input-box">
+                        <label for="contatos">Contatos</label>
+                        <textarea id="contatos" type="text" name="contatos" placeholder="Contatos dos Familiares" value="<?php echo $CONTATOS ?>"></textarea>
+                    </div>
+                    <div class="continue-button ">
+                       <button type="submit" name="gravar" id="gravar">Cadastrar</button>
+                       <button type="submit"><a href="index.php">Voltar</a></button>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
