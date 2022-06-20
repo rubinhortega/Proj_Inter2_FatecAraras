cac15d9b960e33533c67a844d0dee5c8dd59f400
Danielle Ferreira <103910912+DannielleFerreira@users.noreply.github.com>
2022-06-20 13:18:57 -0300

    Update and rename usuariodam.html to usuariodam.php

--- Site/usuariodam.php
+++ Site/usuariodam.php
@@ -1,3 +1,15 @@
+<?php
+// Inicialize a sessão
+session_start();
+ 
+// Verifique se o usuário está logado, se não, redirecione-o para uma página de login
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
